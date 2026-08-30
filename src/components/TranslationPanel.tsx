40943116e605df7b8daf0b9b9c28525402700a5a
JovialDuplex <takeuhduplex2006@gmail.com>
2026-08-30 06:19:01 +0200

    modification des url de traduction

--- src/components/TranslationPanel.tsx
+++ src/components/TranslationPanel.tsx
@@ -49,11 +49,7 @@ export function TranslationPanel() {
       console.log("translation in english...");
       setLoading(true);
       try {
-        const response = await axios.post(fr_en_url, {"text": sourceText}, {
-          headers: {
-            "Content-Type" : "multipart/form-data"
-          }
-        });
+        const response = await axios.post(fr_en_url, {"text": sourceText});
         const data = await response.data;
         setTranslateText(data["translation-text"]);
 
@@ -70,11 +66,7 @@ export function TranslationPanel() {
       console.log("translation in french...");
       try {
         setLoading(true);
-        const response = await axios.post(en_fr_url, {"text": sourceText}, {
-          headers: {
-            "Content-Type" : "multipart/form-data"
-          }
-        });
+        const response = await axios.post(en_fr_url, {"text": sourceText});
         const data = await response.data;
         setTranslateText(data["translation-text"]);
 
